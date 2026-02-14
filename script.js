03ec84b0aab168896b25885fc67f99e65722ac48
gr-pf <237513179+gr-pf@users.noreply.github.com>
2026-02-14 21:45:41 +0100

    Add function getHumanChoice

--- script.js
+++ script.js
@@ -4,4 +4,8 @@ function getComputerChoice() {
   return rpsChoices[Math.floor(Math.random() * 3)];
 }
 
-console.log(getComputerChoice());
+function getHumanChoice() {
+  let userChoice = prompt("Choose between rock, paper and scissors");
+  return userChoice;
+}
+getHumanChoice();
