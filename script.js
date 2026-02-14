78bf76b75ad349af0b33954af47b45669e88384c
gr-pf <237513179+gr-pf@users.noreply.github.com>
2026-02-14 21:57:50 +0100

    Add function playRound

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const rpsChoices = ["Rock", "Paper", "Scissors"];
 
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
   return rpsChoices[Math.floor(Math.random() * 3)];
 }
@@ -8,4 +11,27 @@ function getHumanChoice() {
   let userChoice = prompt("Choose between rock, paper and scissors");
   return userChoice;
 }
-getHumanChoice();
+
+function playRound(humanChoice, computerChoice) {
+  humanChoice = humanChoice.toLowerCase();
+  computerChoice = computerChoice.toLowerCase();
+
+  if (humanChoice === computerChoice) {
+    console.log("It's a tie !");
+  } else if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper")
+  ) {
+    humanScore++;
+    console.log("You Win !");
+  } else {
+    computerScore++;
+    console.log("You Lose !");
+  }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
